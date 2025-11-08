d89cfb985c6a1c7bcc83800b7d3fed7f87345a1d
mkrol <m.krol1914@gmail.com>
2025-11-08 13:35:26 +0100

    rozbudowa apolikacji

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def select_sheet():
     window.title("Sheet z danymi")
     window.geometry("500x750")
 
+
     sheet = pd.read_csv('sheet.csv')
 
     table = ttk.Treeview(window, columns=("kol1", "kol2"))
